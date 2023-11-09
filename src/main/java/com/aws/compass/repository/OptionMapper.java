dca04f9ea1dfc2d42f76ea99d8b8168c5171a3c4
fjfj10 <kimmisuk2218@gmail.com>
2023-11-09 12:53:48 +0900

    Comment: 쓸모없는 주석 삭제

--- src/main/java/com/aws/compass/repository/OptionMapper.java
+++ src/main/java/com/aws/compass/repository/OptionMapper.java
@@ -1,10 +1,7 @@
 package com.aws.compass.repository;
 
-<<<<<<< HEAD
+
 import com.aws.compass.entity.AdministrativeDistrict;
-=======
-import com.aws.compass.entity.AcademyRegistration;
->>>>>>> main
 import com.aws.compass.entity.EducationOffice;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -13,9 +10,6 @@ import java.util.List;
 @Mapper
 public interface OptionMapper {
     public List<EducationOffice> getEducationOfficeList();
-<<<<<<< HEAD
     public List<AdministrativeDistrict> getAdministrativeDistrictList();
-=======
 
->>>>>>> main
 }
