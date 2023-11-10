25d652268d4bbab05a9fe9e94b8f407c4fc727f9
GaYoung Jung <mini3100@naver.com>
2023-11-10 19:14:06 +0900

    Feat: 관리자 마이페이지 학원 등록 승인 기능 구현 완료

--- src/main/java/com/aws/compass/service/AcademyService.java
+++ src/main/java/com/aws/compass/service/AcademyService.java
@@ -1,10 +1,14 @@
 package com.aws.compass.service;
 
 import com.aws.compass.dto.AcademyRegistrationReqDto;
+import com.aws.compass.dto.AwaitingAcademiesRepDto;
 import com.aws.compass.repository.AcademyMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +18,6 @@ public class AcademyService {
 
     @Transactional(rollbackFor = Exception.class)
     public boolean academyRegist(AcademyRegistrationReqDto academyRegistrationReqDto) {
-        System.out.println("service:" + academyRegistrationReqDto);
         return academyMapper.academyRegist(academyRegistrationReqDto.toAcademyRegist()) > 0;
     }
 }
