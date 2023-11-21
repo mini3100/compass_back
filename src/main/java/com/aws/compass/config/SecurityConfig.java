e2091d0485f0de81ca28b3486136740424dd691c
JIWOO\Administrator <jw7812@naver.com>
2023-11-21 16:10:20 +0900

    Feat: 광고 결제 기능 추가 #60

--- src/main/java/com/aws/compass/config/SecurityConfig.java
+++ src/main/java/com/aws/compass/config/SecurityConfig.java
@@ -26,7 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors();            // WebMvcConfig의 CORS 설정을 적용
         http.csrf().disable();  // CSRF 보호 비활성화
         http.authorizeRequests()
-                .antMatchers("/api/auth/**", "/api/option/**", "/api/account/auth/email", "/api/academies", "/api/academy/**", "/api/account/like/**", "/api/review/**")
+                .antMatchers("/api/auth/**", "/api/option/**", "/api/account/auth/email",
+                        "/api/academies", "/api/academy/**", "/api/account/like/**",
+                        "/api/review/**", "/api/purchase/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
