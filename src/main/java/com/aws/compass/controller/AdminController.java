60c5cfe69b8b60cf0e09182c0b2cd34baba1245a
JIWOO\Administrator <jw7812@naver.com>
2023-11-24 15:25:10 +0900

    DOCS: 각 controller 기능 설명 주석 추가 #77

--- src/main/java/com/aws/compass/controller/AdminController.java
+++ src/main/java/com/aws/compass/controller/AdminController.java
@@ -13,16 +13,19 @@ public class AdminController {
 
     private final AdminService adminService;
 
+    // 대기 학원 가져오기
     @GetMapping("/api/admin/academies/awaiting/{page}")
     public ResponseEntity<?> getAwaitingAcademies(@PathVariable int page) {
         return ResponseEntity.ok(adminService.getAwaitingAcademies(page));
     }
 
+    // 학원 승인하기
     @PutMapping("/api/admin/academy/approval")
     public ResponseEntity<?> approvalAcademy(@RequestBody ApprovalAcademyReqDto approvalAcademyReqDto) {
         return ResponseEntity.ok(adminService.approvalAcademy(approvalAcademyReqDto));
     }
 
+    // 학원 승인 거절하기
     @PutMapping("/api/admin/academy/disapproval")
     public ResponseEntity<?> disapprovalAcademy(@RequestBody DisapprovalReqDto disapprovalReqDto) {
         return ResponseEntity.ok(adminService.disapprovalAcademy(disapprovalReqDto));
--- src/main/java/com/aws/compass/controller/AdvertisementController.java
+++ src/main/java/com/aws/compass/controller/AdvertisementController.java
@@ -12,6 +12,7 @@ public class AdvertisementController {
 
     private final AdvertisementService advertisementService;
 
+    // 광고 학원 랜덤 나타내기
     @GetMapping("/api/ad/academies/random")
     public ResponseEntity<?> getRandomAcademies() {
         return ResponseEntity.ok(advertisementService.getADOfRandomAcademies());
